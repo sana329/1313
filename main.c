e2001df3aceb7fa6fe0654f5542c819cc50521d2
sana329 <sahana56113@gmail.com>
2025-05-19 22:19:04 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main() {
     int num1 = 10;
     int num2 = 5;
-    sum = num1 + num2; 
-    printf("Sum: %d\n", sum)
+    int sum = num1 + num2;
+    printf("Sum: %d\n", sum);
     return 0;
 }
